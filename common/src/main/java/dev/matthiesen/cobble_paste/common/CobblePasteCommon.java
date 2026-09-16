f5d21f3d43e6c3f3bda06d0e56091daeb98942f1
Adam Matthiesen <amatthiesen@outlook.com>
2026-09-15 22:01:02 -0700

    Refactor PokePastePartyProvider to Kotlin and update registration method (#9)

--- common/src/main/java/dev/matthiesen/cobble_paste/common/CobblePasteCommon.java
+++ common/src/main/java/dev/matthiesen/cobble_paste/common/CobblePasteCommon.java
@@ -2,9 +2,9 @@ package dev.matthiesen.cobble_paste.common;
 
 import dev.matthiesen.cobble_paste.common.commands.CobblePasteCommands;
 import dev.matthiesen.cobble_paste.common.config.CobblePasteConfig;
-import dev.matthiesen.cobble_paste.common.services.PokePastePartyProvider;
 import dev.matthiesen.cobble_paste.common.registry.PermissionsRegistry;
 import dev.matthiesen.cobble_paste.common.services.preview.PreviewService;
+import dev.matthiesen.cobble_paste.services.PokePastePartyProvider;
 import dev.matthiesen.libs.faststats.Token;
 import dev.matthiesen.matthiesen_core.common.AbstractCommonMod;
 import dev.matthiesen.matthiesen_core.common.api.platform.loader.ModConfigType;
@@ -44,7 +44,7 @@ public final class CobblePasteCommon extends AbstractCommonMod {
        PreviewService.init();
        getCommandsRegistryManager().registerCommand(CobblePasteCommands.CMD);
 
-       PokePastePartyProvider.register();
+       PokePastePartyProvider.Companion.register();
 
        createInfoLog("Initialized");
     }
